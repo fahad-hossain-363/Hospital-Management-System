f9ec62a012ce5a63b5cd714f018567aff8c78205
Sayon <251-35-492@diu.edu.bd>
2025-12-13 19:22:33 +0600

    Add test case

--- tests/test_phone_validator.c
+++ tests/test_phone_validator.c
@@ -4,16 +4,28 @@
 
 int test_validate_validphone() {
     char phone[]="01312381636";
-   int expected=1;
-   int actual=utils_is_valid_phone(phone);
+    int expected=1;
+    int actual=utils_is_valid_phone(phone);
     int succes= actual==expected;
-    printf("actual: %d, expected: %d,succes:%d",actual,expected,actual==expected);
+    printf("\n\nValid phone number: \n");
+    printf("actual: %d, expected: %d,succes:%d\n",actual,expected,actual==expected);
 
 }
+int test_validate_invalidphone() {
+    char phone[]="01112381636";
+    int expected=0;
+    int actual=utils_is_valid_phone(phone);
+    int succes= actual==expected;
+    printf("\nInvalid phone number: \n");
+    printf("actual: %d, expected: %d,succes:%d\n",actual,expected,actual==expected);
+
+}
+
 
 
 
 int main()
 {
     test_validate_validphone();
+    test_validate_invalidphone();
 }
